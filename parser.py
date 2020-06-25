526d798ae2fa29be692145895c8428fe91665934
luluricketts <55892764+luluricketts@users.noreply.github.com>
2020-06-25 15:14:11 -0700

    read file in as chunks

--- parser.py
+++ parser.py
@@ -16,8 +16,10 @@ def load_json(data_folder):
         fxy = doc['overall_freq']['total']
         fxy_s = doc['starred_freq']['total']
 
-        ngd = (max(np.log(fx),np.log(fy)) - np.log(fxy)) / (np.log(N) - min(np.log(fx),np.log(fy)))
-        ngd_s = (max(np.log(fx),np.log(fy)) - np.log(fxy_s)) / (np.log(N) - min(np.log(fx),np.log(fy)))
+        ngd = N if fxy == 0 else ((max(np.log(fx),np.log(fy)) - np.log(fxy)) /\
+                                (np.log(N) - min(np.log(fx),np.log(fy))))
+        ngd_s = N if fxy_s == 0 else ((max(np.log(fx),np.log(fy)) - np.log(fxy_s)) /\
+                                (np.log(N) - min(np.log(fx),np.log(fy))))
         return ngd, ngd_s
 
     co_occur = os.path.join(data_folder, 'summary_CoOccurs_2020.txt')
@@ -25,27 +27,30 @@ def load_json(data_folder):
     assert os.path.exists(co_occur)
     assert os.path.exists(freq)
 
-    df = pd.read_csv(co_occur, sep='|')
     df_freq = pd.read_csv(freq, sep='|')
+    chunks = pd.read_csv(co_occur, sep='|', chunksize=10**7)
 
-    timeFrames = ['MBD', 'MED']
-    for time in timeFrames:
-        loc = 'Freq_' + time
-        loc_s = 'StarFreq_' + time
-        df[loc] = df[df['TimeFrame(SAB)'] == time]['Freq']
-        df[loc_s] = df[df['TimeFrame(SAB)'] == time]['StarFreq(COF)']
-    df.fillna(0, inplace=True)
+    df_list = []
+    for chunk in chunks:
+        timeFrames = ['MBD', 'MED']
+        for time in timeFrames:
+            loc = 'Freq_' + time
+            loc_s = 'StarFreq_' + time
+            chunk[loc] = chunk[chunk['TimeFrame(SAB)'] == time]['Freq']
+            chunk[loc_s] = chunk[chunk['TimeFrame(SAB)'] == time]['StarFreq(COF)']
+        chunk.fillna(0, inplace=True)
 
-    cols = ['Freq', 'StarFreq(COF)', 'Freq_MBD', 'StarFreq_MBD', 'Freq_MED', 'StarFreq_MED']
-    df = df.groupby(['DUI1', 'DUI2', 'CUI1', 'CUI2'])[cols].sum().astype(int).reset_index()
+        id_cols = ['DUI1', 'DUI2', 'CUI1', 'CUI2']
+        cols = ['Freq', 'StarFreq(COF)', 'Freq_MBD', 'StarFreq_MBD', 'Freq_MED', 'StarFreq_MED']
+        df_list.append(chunk.groupby(id_cols)[cols].sum().astype(int).reset_index())
+    df = pd.concat(df_list).groupby(id_cols)[cols].sum().reset_index()
 
     for i,row in df.iterrows():
         doc = {'_id' : '-'.join([row[0], row[1]]),
               'MESH1' : row[0], 'MESH2' : row[1], 'UMLS1' : row[2], 'UMLS2' : row[3],
               'combo' : ['-'.join([i,j]) for i in [row[0],row[2]] for j in [row[1],row[3]]],
               'overall_freq' : {'total' : row[4], 'most_current_10' : row[6], 'most_current_5' : row[8]},
-              'starred_freq' : {'total' : row[5], 'most_current_10' : row[7], 'most_current_5' : row[9]}
-              }
+              'starred_freq' : {'total' : row[5], 'most_current_10' : row[7], 'most_current_5' : row[9]}}
         ngd = get_ngd(doc)
         doc['ngd_overall'] = ngd[0]
         doc['ngd_starred'] = ngd[1]
